8444722a76073ccf733d752b4c67038172fffe29
itaru kitamura <kitaita@hotmail.co.jp>
2021-12-08 23:34:57 +0900

    style: bootstrapとfontを導入。

--- backend/resources/views/components/layout.blade.php
+++ backend/resources/views/components/layout.blade.php
@@ -4,6 +4,12 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!--Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+   <!--Font Awesome5-->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
    <title>My Blog</title>
    <link rel="stylesheet" href="/app.css">
 </head>
