dc93875a20b53c1d20404a5460db05ef0143357d
itaru kitamura <kitaita@hotmail.co.jp>
2021-12-12 22:44:32 +0900

    feat: イベント登録画面を作成。

--- backend/resources/views/event/create.blade.php
+++ backend/resources/views/event/create.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-イベント作成
+{{ Form::open(['action' => 'App\Http\Controllers\EventController@store', 'method' => 'post']) }}
+
+<p>
+{{ Form::label('event_name', 'イベント名') }}
+{{ Form::text('event_name', '', ['id' => 'event_name', 'size' => 50]) }}
+</p>
+
+<p>
+{{ Form::label('start_time', '開始日時') }}
+<input type="datetime-local" id="start_time" name="start_time">
+</p>
+
+<div>
+    {{ Form::reset('リセット', ['class' => 'btn btn-outline-success btn-lg']) }}
+    {{ Form::submit('登録', ['name' => 'regist', 'class' => 'btn btn-success btn-lg']) }}
+</div>
+{{ Form::close() }}
 </x-layout>
