32cbe4c3d814d3d4bd1ea2abbb5e7fc61a448f9d
itaru kitamura <kitaita@hotmail.co.jp>
2022-01-24 22:49:03 +0900

    2022.01.24

--- frontend/workspace/my-project/src/App.vue
+++ frontend/workspace/my-project/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div v-show="message">
+    {{ json }}
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/HelloWorld.vue";
+import axios, { AxiosRequestConfig } from "axios";
 
 @Options({
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private json = "";
+  mounted() {
+    axios.get("http://localhost:8080/api/events").then((response) => {
+      //this.json = JSON.stringify(response.status);
+      this.json = "json予定";
+      alert("api call");
+    });
+  }
+}
 </script>
 
 <style>
